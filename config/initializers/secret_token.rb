1ef961bf090c4d6be33f9e1c2776e7ad1a6469ca
fermin <fermin@huabid.com>
2013-09-16 17:26:13 +0800

    new

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+BidWeibo::Application.config.secret_key_base = '7215a716fbd30c27142f2b1361bc77b6cfe023bfc43aaa2f34c1e67dc40c6ae23c6c129ba2af4aadf53e487b960c1bb1d0e72741a2eb771223b7dfbbb3cdc281'
